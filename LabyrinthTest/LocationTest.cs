cdfb64e80731640b5ac9545dc94bacb087b1e8dd
mgray35 <mac.gray.311@gmail.com>
2020-11-11 12:25:09 -0800

    Adding unit test for Location.

---
+++ LabyrinthTest/LocationTest.cs
@@ -0,0 +1,34 @@
+﻿using Labyrinth;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabyrinthTest
+{
+    [TestClass]
+    class LocationTest
+    {
+        [TestMethod]
+        public void GetNeighborTest()
+        {
+            Location location = new Location();
+            Location neighbor = new Location();
+            int dirInt = Utils.Random.Next(Enum.GetValues(typeof(Direction)).Length);
+            Direction dir = (Direction)dirInt;
+            foreach (Direction d in Enum.GetValues(typeof(Direction)))
+            {
+                Assert.AreEqual(null, location.GetNeighbor(d));
+            }
+
+            location.Neighbors[dirInt] = neighbor;
+            foreach (Direction d in Enum.GetValues(typeof(Direction)))
+            {
+                if (d == dir)
+                    Assert.AreEqual(null, location.GetNeighbor((d)));
+                else
+                    Assert.AreEqual(neighbor, location.GetNeighbor((d)));
+            }
+        }
+    }
+}
